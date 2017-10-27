e116ae91cbfc55e58b061381b9c88fe0777d9e54
Prithviraj singh <prithviraj86@gmail.com>
2017-10-27 17:40:00 +0530

    Change in producttest file

--- Newtest/productTest.php
+++ Newtest/productTest.php
@@ -8,18 +8,18 @@ class ProductTest extends PHPUnit_Framework_TestCase
         require_once 'User.php';
         require_once 'Product.php';
     }
-    public function testSetGetUserData()
+    public function testSetGetUserProductData()
     {
         $user = new User(1, 'Prithviraj');
 
         $this->product=new Product($user,array('id'=>1,'name'=>'Hp Printer'));
-
+        //Test for check Product id empty and equal
         $this->assertEquals(1, $this->product->getPid());
         $this->assertEmpty($this->product->getPid());
-
+        //Test for check Product name equal
         $this->assertEquals('Hp Printer', $this->product->getPname());
         //$this->assertEmpty($this->product->getPname());
-
+        //Test for check User id and username equal
         $this->assertEquals(1, $this->product->getUid());
         $this->assertEquals('Prithviraj', $this->product->getUname());
 
